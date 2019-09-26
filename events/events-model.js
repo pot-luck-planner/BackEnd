db3aea959d07da8b314bb75f8679ef94179befd6
Jonathon Green <jonathongre@gmail.com>
2019-09-26 15:58:44 -0500

    change id to invite_id on GET my invites

--- events/events-model.js
+++ events/events-model.js
@@ -75,8 +75,8 @@ function myInvites(id) {
   return db('invites as i')
     .innerJoin('accounts as a', 'i.account_id', '=', 'a.id')
     .innerJoin('events as e', 'i.event_id', '=', 'e.id')
-    .select('i.id', 'i.event_id', 'e.name', 'e.date', 'e.time', 'e.location', 'e.host_id', 'e.host_name as host', 'i.account_id', 'a.username', 'a.firstname', 'a.lastname', 'i.food', 'i.notes', 'i.rsvp')
-    .where({ account_id: id} )
+    .select('i.id as invite_id', 'i.event_id', 'e.name', 'e.date', 'e.time', 'e.location', 'e.host_id', 'e.host_name as host', 'i.account_id', 'a.username', 'a.firstname', 'a.lastname', 'i.food', 'i.notes', 'i.rsvp')
+    .where( { account_id: id} )
 }
  
 //Update invites
